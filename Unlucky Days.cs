39b31aafbb79c68aff763c2649d252b3b7463f0a
mephistachio <47401980+mephistachio@users.noreply.github.com>
2019-02-27 21:49:06 +0100

    Add files via upload

---
+++ Unlucky Days.cs
@@ -0,0 +1,20 @@
+﻿public class Kata
+{
+    public static int Main()
+    {
+        var res = UnluckyDays(2017);
+        return res;
+    }
+
+    public static int UnluckyDays(int year)
+    {
+        var count = 0;
+        for (int month = 1; month <= 12; month++)
+        {
+            var date = new DateTime(year, month, 13);
+            if (date.DayOfWeek == DayOfWeek.Friday)
+                count++;
+        }
+        return count;
+    }
+}
